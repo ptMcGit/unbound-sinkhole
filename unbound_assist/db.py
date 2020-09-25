1c51fba804185a3565c30529be496c37c532a824
PM <13983935+ptMcGit@users.noreply.github.com>
2020-09-24 23:48:17 -0400

    update db module to return records in a generator

---
+++ unbound_assist/db.py
@@ -0,0 +1,89 @@
+"""
+Sqlite3 functions.
+
+The sqlite3 functions like tuples so most of
+the functions that modify the DB expect lists
+of tuples of the form (<ip_addres>, <url>).
+
+"""
+
+import sqlite3
+import pathlib
+
+sql_db = "./unbound-sinkhole.db"
+db_sinkhole_table = "list"
+
+def _set_blacklist(blacklist):
+    return ("TRUE" if blacklist else "FALSE")
+
+def init_db():
+    """ Initialize the database,
+    or return if db already exists """
+    if pathlib.Path(sql_db).exists():
+        return
+
+    with sqlite3.connect(sql_db) as con:
+        con.execute('''CREATE TABLE {0} (
+        id INTEGER PRIMARY KEY,
+        sinkhole BOOLEAN DEFAULT TRUE,
+        ip_addr VARCHAR(64) NOT NULL,
+        url VARCHAR(64) NOT NULL,
+        UNIQUE(ip_addr, url))'''.format(db_sinkhole_table))
+
+def update_records(records, blacklist=True):
+    """ Update the db with records.
+
+    Add or update an individual record or a list of records.
+
+    Args:
+        records: list of records to update
+        blacklist: whether the records should be added to the blacklist
+
+    Raises:
+        Any error that is not sqlite3.IntegrityError.
+    """
+
+    blacklist = _set_blacklist(blacklist)
+    with sqlite3.connect(sql_db) as con:
+        for r in records:
+            print((blacklist,) + r)
+            try:
+                con.execute('''INSERT INTO {0}
+                (sinkhole, ip_addr, url) VALUES(?, ?, ?)'''.format(db_sinkhole_table, blacklist), (blacklist,) + r)
+            except sqlite3.IntegrityError as e:
+                con.execute('''UPDATE {0}
+                SET sinkhole = "{1}"
+                WHERE (ip_addr = "{2}" AND url = "{3}")'''.format(db_sinkhole_table, blacklist, r[0], r[1]))
+
+def delete_records(records):
+    """ Delete records provided from the DB.
+
+    Args:
+        records: list of records to delete.
+
+    Raises:
+        Any error that is not sqlite3.IntegrityError.
+    """
+
+    with sqlite3.connect(sql_db) as con:
+        for r in records:
+            con.execute('''DELETE FROM {0}
+            WHERE (ip_addr = "{1}" AND url = "{2}")'''.format(db_sinkhole_table,
+                                                         r[0],
+                                                         r[1]))
+
+def purge_db():
+    """ Purge all the records from the db.
+    """
+    with sqlite3.connect(sql_db) as con:
+        con.execute("DELETE FROM {0}".format(db_sinkhole_table))
+
+def get_blacklist():
+    """ Get all blacklist records.
+    Returns:
+        List containing all blacklist records.
+    """
+    with sqlite3.connect(sql_db) as con:
+        r = con.execute('''SELECT ip_addr, url FROM {0}
+        WHERE sinkhole = "TRUE"'''.format(db_sinkhole_table))
+    return r.fetchall()
---
+++ unbound_assist/tests/test_db.py
@@ -0,0 +1,93 @@
+import unittest
+import db as db
+import sqlite3
+
+class TestDb(unittest.TestCase):
+    def setUp(self):
+        db.init_db()
+
+    def tearDown(self):
+        db.purge_db()
+
+    def get_records(self):
+        ''' Helper method to get all records
+        '''
+        with sqlite3.connect(db.sql_db) as con:
+            return con.execute("SELECT * FROM {0}".format(db.db_sinkhole_table)).fetchall()
+
+    def test_update_records(self):
+        ''' Various checks for method for
+        inserting records. '''
+
+        records1 = [("0.0.0.0", "yahoo.com.")]
+
+        db.update_records(records1, blacklist=True)
+        # insert duplicate
+        db.update_records(records1, blacklist=True)
+
+        r = self.get_records()
+        self.assertEqual(len(r), 1)
+        self.assertEqual(r[0][1], 'TRUE')
+
+        # update sinkhole boolean
+        db.update_records(records1, blacklist=False)
+
+        r = self.get_records()
+        self.assertEqual(len(r), 1)
+        self.assertEqual(r[0][1], 'FALSE')
+
+        # attempt insert record with same ip, different host
+        db.update_records([("0.0.0.0", "google.com.")]
+                          , blacklist=False)
+
+        r = self.get_records()
+        self.assertEqual(len(r), 2)
+
+        # record with different ip same host
+        db.update_records([("1.1.1.1", "google.com.")],
+                          blacklist=True)
+
+        r = self.get_records()
+        self.assertEqual(len(r), 3)
+
+    def test_delete_records(self):
+        ''' Test deleting records.
+        '''
+        a = ("0.0.0.0", "a.com")
+        b = ("1.1.1.1", "b.com")
+        c = ("2.2.2.2", "c.com")
+
+        db.update_records([a,
+                           b,
+                           c],
+                          blacklist=True)
+
+        db.delete_records([a, b])
+        r = self.get_records()
+        self.assertEqual(len(r), 1)
+
+        db.delete_records([c])
+
+        # delete twice
+        db.delete_records([c])
+        r = self.get_records()
+        self.assertEqual(len(r), 0)
+
+
+    def test_get_blacklist(self):
+        ''' Test getting the blacklist.
+        '''
+        a = ("0.0.0.0", "a.com")
+        b = ("1.1.1.1", "b.com")
+        db.update_records([a,
+                           b],
+                          blacklist=True)
+        db.update_records([("2.2.2.2", "c.com")], blacklist=False)
+        r = list(db.get_blacklist())
+
+        self.assertEqual(r[0], a)
+        self.assertEqual(r[1], b)
+        self.assertEqual(len(r), 2)
+
+if __name__ == '__main__':
+    unittest.main()
