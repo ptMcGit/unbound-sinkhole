38247d2d0829eb7f197ee93d76faacc9e25f15dc
PM <13983935+ptMcGit@users.noreply.github.com>
2020-09-27 11:23:20 -0400

    Add parsing module with tests

---
+++ unbound_assist/parsing.py
@@ -0,0 +1,121 @@
+"""
+Module for
+
+- parsing records from files
+- validating records
+
+The expected format for records is
+
+    <ip address> <url>
+"""
+
+import ipaddress
+from pathlib import Path
+import re
+from urllib.parse import urlparse
+
+def _check_hostname(hostname):
+    """Thorough hostname validation.
+
+    Predicate function that checks hostname according
+    to the prevailing Internet specs.
+
+    Not mine - from stackoverflow
+
+    Args:
+        hostname: hostname to check
+
+    Returns: True if hostname is valid, else False.
+    """
+    if len(hostname) > 255:
+        return False
+    if hostname[-1] == ".":
+        hostname = hostname[:-1] # strip exactly one dot from the right, if present
+    allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in hostname.split("."))
+
+def _check_ip_addr(address):
+    """Check if IPv4/6 address is valid.
+
+    Predicate function that checks if IP address
+    is valid.
+
+    Args:
+        address: The address to check.
+
+    Returns: True if hostname is valid, else False.
+    """
+    try:
+        return ipaddress.ip_address(address)
+    except ValueError as e:
+        return False
+
+def record_check(record):
+    """Check if a record is valid or not.
+
+    Args:
+        record: The record to check.
+
+    Returns: The record when valid, else None.
+    """
+    # remove any successive whitespace, and strip sides
+    record = re.sub('^(\s*)(\S+)(\s+)(\S+)(\s*)$',
+                    '\\2 \\4',
+                    record).split(' ')
+
+    if not _check_ip_addr(record[0]):
+        return None
+    if not _check_hostname(record[1]):
+        return None
+    return record
+
+def _gen_records(filelist):
+    """A generator function for records.
+
+    Take a list of files and create a
+    generator that furnishes records.
+
+    Args:
+        filelist: list from which records are drawn.
+
+    Returns: yields records.
+    """
+    # if no errors
+    for item in filelist:
+        with open(item, 'r') as f:
+            for line in f:
+                yield tuple(record_check(line))
+
+
+def process_files(filelist):
+    """Checks files provided then generates records.
+
+    The files provided are checked along with each
+    of the records.
+
+    If all of the file and records are valid a
+    generator is returned.
+
+    Args:
+        filelist: files to process.
+
+    Returns: a generator containing records.
+
+    Raises: Exception when a path doesn't exist,
+        or when a bad record is found.
+    """
+    # evaluate each file and each record first
+    for item in filelist:
+        print(item)
+        if not Path(item).exists():
+            raise Exception("path does not exist")
+        with open(item, 'r') as f:
+            c = 1
+            for line in f:
+                record_status = record_check(line)
+                if not record_status:
+                    # TODO where is the bad record
+                    raise Exception('bad record found at line {0} in file {1}'.format(c, item))
+                c += 1
+
+    return _gen_records(filelist)
---
+++ unbound_assist/tests/test_parsing.py
@@ -0,0 +1,59 @@
+import parsing
+from pathlib import Path
+import unittest
+
+class TestParsing(unittest.TestCase):
+
+    def setUp(self):
+        return
+
+    def tearDown(self):
+        return
+
+    def test_record_check(self):
+        # good ipv4 record
+        r = "0.0.0.0 www.example.com"
+        self.assertIsNotNone(parsing.record_check(r))
+
+        # bad ipv4 record
+        r = "0.0.0 www.example.com"
+        self.assertIsNone(parsing.record_check(r))
+
+        # bad ipv6 record
+        r = "::: www.example.com"
+        self.assertIsNone(parsing.record_check(r))
+
+        # bad hostnames
+        r = "0.0.0.0 www._.com"
+        self.assertIsNone(parsing.record_check(r))
+        r = "0.0.0.0 www._.com/home"
+        self.assertIsNone(parsing.record_check(r))
+        r = "0.0.0.0 www._.com/home/?s=5"
+        self.assertIsNone(parsing.record_check(r))
+
+    def test_process_files(self):
+        # check ok files
+        l = list(parsing.process_files(['tests/inputs/records_file_ok',
+                                   'tests/inputs/records_file_ok2']))
+
+        self.assertEqual([('0.0.0.0', 'bad-site.com'),
+                          ('181.16.9.1', 'www.evil-site.com'),
+                          ('5666::1112', 'www.scary-site.co.us'),
+                          ('0.0.0.0', 'ok-site.com')],
+                         l)
+
+        # check file that doesn't exist
+        non_file = "tests/inputs/nonexistent_file"
+        self.assertFalse(Path(non_file).exists())
+
+        with self.assertRaises(Exception):
+            parsing.process_files(non_file)
+
+        # check file with bad records
+        bad_records_file = 'tests/inputs/records_file_bad'
+        with self.assertRaises(Exception):
+            parsing.process_files(bad_records__file)
+
+
+if __name__ == '__main__':
+    unittest.main()
