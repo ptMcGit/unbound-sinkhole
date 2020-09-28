44c5b3b84946704f2d47beaeaf247f5db079ecfc
PM <13983935+ptMcGit@users.noreply.github.com>
2020-09-28 16:47:59 -0400

    Migrate from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,23 +1,18 @@
 """ Setup file.
 """
 
-import setuptools
+# import setuptools
 
-if __name__ == '__main__':
-    setuptools.setup(
-        name='unbound-sinkhole',
-        version='0.1',
-
-        packages=setuptools.find_packages(),
-
-        entry_points={
-            'console_scripts': [
-                'unbound-sinkhole = unbound_sinkhole.cli:main',
-            ]
-        },
-
-        test_suite='unbound_sinkhole.tests',
-        description='Script for sinkholing hosts using Unbound.',
-
-        license='MIT'
-    )
+# if __name__ == '__main__':
+#     setuptools.setup(
+#         name='unbound-sinkhole',
+#         version='0.1',
+#         packages=setuptools.find_packages(),
+#         entry_points={
+#             'console_scripts': [
+#                 'unbound-sinkhole = unbound_sinkhole.cli:main',
+#             ]
+#         },
+#         test_suite='unbound_sinkhole.tests',
+#         description='Script for sinkholing hosts using Unbound.'
+#     )
